e31c5f5c1eab996cd5bd2d3fb710296627042e9e
hendrikniemann <h.neverman@web.de>
2020-08-15 15:57:53 +0200

    Create solutions for backend

--- backend/src/graphql/types.js
+++ backend/src/graphql/types.js
@@ -1,8 +1,85 @@
-import { GraphQLObjectType } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLID,
+} from "graphql";
+import { PetModel } from "../models";
+
+export const Animal = new GraphQLEnumType({
+  name: "Animal",
+  description:
+    "In our pet shelter we house dogs, cats and rabbits. This type is used to reference one of their types.",
+  values: {
+    DOG: { value: "DOG" },
+    CAT: { value: "CAT" },
+    RABBIT: { value: "RABBIT" },
+  },
+});
+
+export const Pet = new GraphQLObjectType({
+  name: "Pet",
+  description: "This is a pet that is or was a guest in our shelter.",
+  fields: {
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "The name of this pet.",
+    },
+    animal: {
+      type: new GraphQLNonNull(Animal),
+      resolve(parent, args, context, info) {
+        return parent.type;
+      },
+    },
+    breed: {
+      type: GraphQLString,
+    },
+    height: {
+      type: GraphQLInt,
+    },
+  },
+});
 
 export const Query = new GraphQLObjectType({
   name: "Query",
   fields: {
-    // Exercise 4: Create your first hello world query
+    hello: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "This field greets everyone.",
+      args: {
+        firstName: {
+          type: GraphQLString,
+          description: "The name of the person to greet.",
+        },
+      },
+      resolve: (parent, args, context, info) => {
+        if (args.firstName) {
+          return `Hello ${args.firstName}!`;
+        }
+        return `Hello World!`;
+      },
+    },
+    pet: {
+      type: Pet,
+      description:
+        "Waldo is the shelters very own dog and not up for adoption!",
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+      },
+      resolve(parent, args, context, info) {
+        return PetModel.findById(args.id);
+      },
+    },
+    pets: {
+      type: new GraphQLList(Pet),
+      resolve() {
+        return PetModel.find();
+      },
+    },
   },
 });
--- backend/src/models.js
+++ backend/src/models.js
@@ -1,6 +1,22 @@
-import { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-export const PetModel = new Schema({
-  // Exercise 3: Fill out the properties of the schema. You can find more information on how to
-  //             define a schema in the docs: https://mongoosejs.com/docs/guide.html#definition
+export const PetSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["DOG", "CAT", "RABBIT"],
+    required: true,
+  },
+  adopted: {
+    type: Boolean,
+    required: true,
+  },
+  picture: String,
+  breed: String,
+  height: Number,
 });
+
+export const PetModel = mongoose.model("Pet", PetSchema);
